634be8eda34062f36ffb735e72963ebc83e5bd9e
czy23333 <554550216@qq.com>
2020-08-17 15:21:55 +0800

    初步完成数据传输

--- Camera/raspberry_camara.py
+++ Camera/raspberry_camara.py
@@ -1,14 +1,18 @@
 import os
 import socket
-import server
-import sys
+import threading
 import time
-import json
 
 # server_address = "192.168.0."
 
 
-def get():  # 获取图片，这里是jpg格式的图片
+# 打开摄像头
+def open_camera():
+    os.system('sh startup.sh')
+
+
+# 获取图片，这里是jpg格式的图片
+def get():
     import requests
     try:
         img = requests.get('http://192.168.0.111:8080/?action=snapshot&n=1', timeout=1).content
@@ -20,32 +24,8 @@ def get():  # 获取图片，这里是jpg格式的图片
         print('get snapshot fail')
 
 
-def send_to_sever(img, sensor, sock, t):
-
-
-    data = []
-    t = bytes(t)
-    interval = b'0000000'
-    data = bytes(img) + interval + bytes(sensor) + interval + t + interval
-    while True:
-        # 发送前确认发送次数
-        length = len(data)
-        # s.connect(server_address)
-        sock.sendall("start".encode(encoding="UTF-8"))
-        # start = time.time()
-        recieve = sock.recv(1024)
-        if recieve == b'OK':  # 发送前确认服务器OK
-            sock.sendall(str(length).encode(encoding="UTF-8"))
-            recieve = sock.recv(1024)
-        if recieve == b'YES':  # 发送前确认服务器收到长度信息
-            already = 0
-            while (length - already) > 0:  # 开始传送
-                send_data = data[already:already+1024]
-                sock.sendall(send_data)
-                already += 1024
-
-
-def get_sensor_value(client_socket):  # 获取传感器数据
+# 获取传感器数据
+def get_sensor_value(client_socket):
 
     client_socket.sendall("start".encode(encoding="UTF-8"))
     recv_data = client_socket.recv(1024)
@@ -57,27 +37,115 @@ def get_sensor_value(client_socket):  # 获取传感器数据
         recv_data = client_socket.recv(1024)
     return recv_data
 
+# t = bytes(t)
+# interval = b'0000000'
+# data = bytes(img) + interval + bytes(sensor) + interval + t + interval
 
-def open_camera():  # 打开摄像头
-    os.system('sh startup.sh')
+
+def sendDataClient():
+    # 准备socket
+    serverAddress = ('127.0.0.1', 8081)
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    print("等待连接....")
+    clientSocket.connect(serverAddress)  # 建立连接
+
+    # 互相发送Hello消息
+    clientSocket.sendall(b'Hello\n')
+    data = b''
+    while True:
+        recvData = clientSocket.recv(1024)
+        data += recvData
+        if data.endswith(b'\n'):
+            break
+
+    while True:
+        # TODO 获取数据
+        sendData = b'datadatadatadatadatadatadatadatadata'
+        length = len(sendData)
+
+        # 发送数据长度
+        clientSocket.sendall(str(length).encode(encoding="UTF-8") + b'\n')
+        data = b''
+        while True:
+            recvData = clientSocket.recv(1024)
+            data += recvData
+            if data.endswith(b'\n'):
+                break
+
+        # 发送数据
+        clientSocket.sendall(sendData)
+        data = b''
+        while True:
+            recvData = clientSocket.recv(1024)
+            data += recvData
+            if data.endswith(b'\n'):
+                break
+
+        # 暂停
+        time.sleep(1)
+
+
+def recvDataClient():
+    # 准备socket
+    serverAddress = ('127.0.0.1', 8082)
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    print("等待连接....")
+    clientSocket.connect(serverAddress)  # 建立连接
+
+
+    while True:
+        # 接收命令
+        recvData = b''
+        while True:
+            data = clientSocket.recv(1024)
+            recvData += data
+            if recvData.endswith(b'\n'):
+                break
+        clientSocket.sendall(b'OK\n')
+        if recvData == b'Hello\n':
+            print('server:Hello')
+            continue
+        else:
+            length = int(recvData[0:len(recvData) - 1])
+            print('要传入的数据大小为：' + str(length))
+
+        # 接收数据
+        recvData = b''
+        while length > 0:
+            data = clientSocket.recv(1024)
+            length -= len(data)
+            recvData += data
+        clientSocket.sendall(b'OK\n')
+        print("传入的数据" + str(recvData))
+
+        # TODO 处理数据
 
 
 if __name__ == '__main__':
-    isStop = True
-    # 连接设备端——相对于设备端，这是服务器
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind(("192.168.0.109", 8082))
-    serverSocket.listen(5)
-    print("等待连接")
-    client_socket, client_info = serverSocket.accept()
-    # 连接服务器——相对于云端，这是客户端
-    server_address = ('127.0.0.1', 8081)  # 服务器地址
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 初始化socket
-    s.connect(server_address)  # 建立连接
-    # 发送数据
-    i = 0
-    while isStop:
-        i += 1
-        send_to_sever(get(), get_sensor_value(client_socket), s, i)
-        if i == 65535:  # 对齐数据，防止网络传输过程中顺序错乱
-            i = 0
+    recvDataThread = threading.Thread(target=recvDataClient)
+    recvDataThread.start()
+    sendDataThread = threading.Thread(target=sendDataClient)
+    sendDataThread.start()
+    # isStop = True
+    # # 连接设备端——相对于设备端，这是服务器
+    # serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # serverSocket.bind(("127.0.0.1", 8082))
+    # serverSocket.listen(5)
+    # print("等待连接")
+    # client_socket, client_info = serverSocket.accept()
+    # # 连接服务器——相对于云端，这是客户端
+    # server_address = ('127.0.0.1', 8081)  # 服务器地址
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 初始化socket
+    # s.connect(server_address)  # 建立连接
+    # # 发送数据
+    # i = 0
+    # while isStop:
+    #     i += 1
+    #     sendDataToSever(get(), get_sensor_value(client_socket), s, i)
+    #     if i == 65535:  # 对齐数据，防止网络传输过程中顺序错乱
+    #         i = 0
+
+
+
--- Camera/server.py
+++ Camera/server.py
@@ -1,26 +1,126 @@
 import socket
 import math
+import time
+import queue
+import threading
+
+# 存储接收到的传感器数据
+sensorDataQueue = queue.Queue()
+handleDataQueue = queue.Queue()
 
 
-def server():
-    server_address = ("127.0.0.1", 8081)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(server_address)
-    server_socket.listen(5)
-    clientSocket, clientInfo = server_socket.accept()
-    recieve_data = b''
-    recv_data = clientSocket.recv(1024)
-    if recv_data == b'start':
-        clientSocket.sendall("OK".encode(encoding="UTF-8"))
-        recv_data = clientSocket.recv(1024)
-    if len(recv_data) > 0:
-        clientSocket.sendall("YES".encode(encoding="UTF-8"))
-        round_times = math.ceil(int(recv_data)/1024)
-        for i in range(round_times):
-            recv_data = clientSocket.recv(1024)
-            recieve_data = recieve_data + bytes(recv_data)
-        print(recieve_data)
+def recvDataServer():
+    # 准备socket
+    serverAddress = ("127.0.0.1", 8081)
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.bind(serverAddress)
+
+    while True:
+        # 等待客户端连接
+        print("recvDataServer等待连接...")
+        serverSocket.listen(5)
+        tcpSocket, clientInfo = serverSocket.accept()
+        print('连接成功')
+
+        # 目前只考虑一个客户端，所以此处不使用多线程
+        while True:
+            recvData = b''
+            # 接收命令
+            while True:
+                data = tcpSocket.recv(1024)
+                recvData += data
+                if recvData.endswith(b'\n'):
+                    break
+            tcpSocket.sendall(b'OK\n')
+            if recvData == b'Hello\n':
+                print('client:Hello')
+                continue
+            else:
+                length = int(recvData[0:len(recvData) - 1])
+                print('要传入的数据大小为：' + str(length))
+
+            recvData = b''
+            # 接收数据
+            while length > 0:
+                data = tcpSocket.recv(1024)
+                length -= len(data)
+                recvData += data
+            tcpSocket.sendall(b'OK\n')
+            print("传入的数据" + str(recvData))
+
+            # TODO 处理数据
+            sensorDataQueue.put(recvData)
+
+
+def sendDataServer():
+    # 准备socket
+    serverAddress = ("127.0.0.1", 8082)
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.bind(serverAddress)
+
+    while True:
+        # 等待客户端连接
+        print("sendDataServer等待连接...")
+        serverSocket.listen(5)
+        tcpSocket, clientInfo = serverSocket.accept()
+        print('连接成功')
+
+        tcpSocket.sendall(b'Hello\n')
+        recvData = b''
+        while True:
+            data = tcpSocket.recv(1024)
+            recvData += data
+            if recvData.endswith(b'\n'):
+                break
+
+        while True:
+            sendData = handleDataQueue.get()
+            print(sendData)
+            length = len(sendData)
+
+            # 发送数据长度
+            tcpSocket.sendall(str(length).encode("UTF-8") + b"\n")
+            recvData = b''
+            while True:
+                data = tcpSocket.recv(1024)
+                recvData += data
+                if recvData.endswith(b'\n'):
+                    break
+
+            # 发送所有数据
+            tcpSocket.sendall(sendData)
+            recvData = b''
+            while True:
+                data = tcpSocket.recv(1024)
+                recvData += data
+                if recvData.endswith(b'\n'):
+                    break
+
+            time.sleep(1)
+
+
+def handleDataProc():
+    while True:
+        rawData = sensorDataQueue.get()
+        handleData = (str(rawData) + str(time.time())).encode("UTF-8")
+        handleDataQueue.put(handleData)
 
 
 if __name__ == "__main__":
-    server()
+    sendDataThread = threading.Thread(target=sendDataServer)
+    sendDataThread.start()
+    recvDataThread = threading.Thread(target=recvDataServer)
+    recvDataThread.start()
+    handleDataThread = threading.Thread(target=handleDataProc)
+    handleDataThread.start()
+
+
+
+
+
+
+
+
+
+
+
